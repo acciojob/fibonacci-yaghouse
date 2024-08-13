de9a120a1742a6449e7ca8ec3f56a0d55ba1478d
YaGhouse <98881486+yaghouse@users.noreply.github.com>
2024-08-13 21:34:22 +0530

    created a submission

--- script.js
+++ script.js
@@ -1,5 +1,20 @@
 function fibonacci(num) {
-// your code here
+    if (num === 1) {
+        return 0;
+    } else if (num === 2) {
+        return 1;
+    }
+
+    let a = 0, b = 1;
+
+    for (let i = 2; i < num; i++) {
+        let temp = b;
+        b = a + b;
+        a = temp;
+    }
+
+    return b;
 }
 
 module.exports = fibonacci;
+
